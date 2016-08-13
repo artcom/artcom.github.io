132cd557ed8e7de30e5fd38e7d5e6b2e9a144185
dirk lüsebrink <dirk.luesebrink@gmail.com>
2016-08-13 11:20:59 +0200

    ruby styled

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,3 +1,6 @@
+#
+# src: https://gist.github.com/joelverhagen/1805814
+# 
 class YouTube < Liquid::Tag
   Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
@@ -6,22 +9,21 @@ class YouTube < Liquid::Tag
 
     if markup =~ Syntax then
       @id = $1
-
-      if $2.nil? then
-          @width = 560
-          @height = 420
-      else
-          @width = $2.to_i
-          @height = $3.to_i
-      end
-    else
       raise "No YouTube ID provided in the \"youtube\" tag"
+    end
+
+    if $2.nil? then
+      @width = 560
+      @height = 420
+    else
+      @width = $2.to_i
+      @height = $3.to_i
     end
   end
 
   def render(context)
     # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+    %{<iframe width="#{@width}" height="#{@height}" src="http://www.youtube.com/embed/#{@id}?color=white&theme=light"></iframe>}
   end
 
   Liquid::Template.register_tag "youtube", self
